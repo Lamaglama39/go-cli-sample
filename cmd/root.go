dad20508e4a8559591dbde364a95f0b04076e430
Lamaglama39 <lamaglama621@gmail.com>
2024-10-05 21:35:27 +0900

    :construction: root.go test

--- cmd/root.go
+++ cmd/root.go
@@ -23,7 +23,7 @@ package cmd
 
 import (
 	"os"
-
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
@@ -42,6 +42,9 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello World!")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
